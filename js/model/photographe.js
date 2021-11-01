0a3ca34ca6aa3a396986fd4496fc0072a2d86484
Nicolas <nicolas.manhaeve@gmail.com>
2021-11-01 17:59:45 +0100

    Update photographe.js

--- js/model/photographe.js
+++ js/model/photographe.js
@@ -1,7 +1,5 @@
 export default class Photographe {
-  constructor({
-    name, city, country, tags, tagline, price, portrait,
-  }) {
+  constructor({name, city, country, tags, tagline, price, portrait}) {
     this.name = name;
     this.city = city;
     this.country = country;
@@ -11,25 +9,24 @@ export default class Photographe {
     this.portrait = portrait;
 
     this.createPhotographe = () => new Photographe();
+    
     document.title = `Profil de ${this.name}`;
-    document.getElementsByClassName('photographe-name')[0].innerText = this.name;
-    document.getElementsByClassName(
-      'photographe-city',
-    )[0].innerText = `${this.city},${this.country}`;
-    document.getElementsByClassName(
-      'photographe-line',
-    )[0].innerText = this.tagline;
+    
+    document.querySelector('.photographe-name').innerText = this.name;
+    
+    document.querySelector('.photographe-city').innerText = `${this.city},${this.country}`;
+    
+    document.querySelector('.photographe-line').innerText = this.tagline;
+    
     this.tags.forEach((tag) => {
-      const span = document.createElement('span');
-      span.className = 'tags';
-      span.innerText = `#${tag}`;
-      document.getElementsByClassName('photographe-tags-container')[0].appendChild(span);
+      //https://developer.mozilla.org/fr/docs/Web/API/Element/insertAdjacentHTML
+      document.querySelector('.photographe-tags-container').insertAdjacentHTML('beforeend', `<span class="tags">#${tag}</span>`)
     });
-    document.getElementsByClassName(
-      'photographe-img',
-    )[0].src = `./images/PhotographersID/${this.portrait}`;
-    document.getElementsByClassName('prix')[0].innerText = `${price}€/jour`;
-    const formTitre = document.querySelector('.form-titre');
-    formTitre.innerHTML = `Contactez-moi<br/>${this.name}`;
+    
+    document.querySelector('.photographe-img').src = `./images/PhotographersID/${this.portrait}`;
+    
+    document.querySelector('.prix').innerText = `${price}€/jour`;
+    
+    document.querySelector('.form-titre').innerHTML = `Contactez-moi<br/>${this.name}`;
   }
 }
